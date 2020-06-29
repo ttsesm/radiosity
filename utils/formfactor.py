fixed ray casting with embree and optimized matrix rotation estimation

utils/formfactor.py
@@ -6,8 +6,10 @@ from multiprocessing.dummy import Pool as ThreadPool
 # from pathos.multiprocessing import ProcessingPool as Pool
 
 import numpy as np
-from numpy.core.umath_tests import inner1d
+import numpy_indexed as npi
+# from numpy.core.umath_tests import inner1d
 
+import time
 import open3d as o3d
 
 import vtk
@@ -18,6 +20,7 @@ from utils.triangle import Triangle, vectorize, distance
 from utils import Isocell
 import trimesh
 import pyembree
+# from pyoctree import pyoctree as ot
 import vtkplotter as vp
 
 from utils import rotation as r
@@ -69,7 +72,8 @@ class FormFactor(object):
         # # test1 = r.vvrotvec(self.mesh.face_normals[1,:], [0, 0, 1])
         # test1 = r.vvrotvec(face_normals[591:593,:], [0, 0, 1])
         # test = r.vvrotvec(face_normals, [0, 0, 1])
-        rotation_matrices = r.vrrotvec2mat(face_normals, [0, 0, 1])
+        # rotation_matrices = r.vrrotvec2mat(face_normals, [0, 0, 1])
+        rotation_matrices = r.rotation_matrices_from_vectors(face_normals, [0, 0, 1])
 
         # drays = np.einsum('ijk,ak->iak', rotation_matrices, self.isocell.points)
         # drays = np.einsum('ijj,aj->iaj', rotation_matrices, self.isocell.points)
@@ -85,7 +89,45 @@ class FormFactor(object):
         origins = np.repeat(origins, self.isocell.points.shape[0], axis=0)
         # origins = np.tile(np.expand_dims(start_points, 0), (drays.shape[0], 1)) + offset
 
+        # tree = ot.PyOctree(self.mesh.vertices.copy(order='C'), self.mesh.faces.copy(order='C').astype(np.int32))
+        # rayList = np.array([origins, drays.reshape(-1, 3)], dtype=np.float32)
+        # startPoint = [0.0, 0.0, 0.0]
+        # endPoint = [0.0, 0.0, 1.0]
+        # rayList1 = np.array([[startPoint, endPoint]], dtype=np.float32)
+        # intersectionFound = tree.rayIntersection(rayList)
+
+        # start casting and find intersection points, rays and faces
+        start = time.time()
         intersection_points, index_ray, index_tri = self.mesh.ray.intersects_location(origins, drays.reshape(-1, 3), multiple_hits=False)
+        end = time.time()
+        print(end - start)
+        # tree = ot.PyOctree(vertices.copy(order='C'), faces.copy(order='C').astype(np.int32))
+
+        # find the indices of rays that did not intersect to any face and recover the size of the total casted rays
+        no_intersection_rays = np.arange(origins.shape[0])
+        idxs_of_no_intersection_rays = no_intersection_rays[~np.isin(np.arange(no_intersection_rays.size), index_ray)]
+
+        # if there are no intersection rays, adjust sizes in the output
+        if idxs_of_no_intersection_rays.any():
+            index_ray = np.insert(index_ray, idxs_of_no_intersection_rays, -1)
+            index_tri = np.insert(index_tri, idxs_of_no_intersection_rays, -1)
+            intersection_points = np.insert(intersection_points, idxs_of_no_intersection_rays, -np.inf, axis=0)
+
+        # eq = npi.group_by(origins[index_ray])
+
+        # locs = trimesh.points.PointCloud(intersection_points)
+
+        # render the result with vtkplotter
+        axes = vp.addons.buildAxes(vp.trimesh2vtk(self.mesh), c='k', zxGrid2=True)
+        rays = vp.Lines(origins[0:1083,:], drays[0,0:1083,:].reshape(-1,3)+origins[0:1083,:], c='b', scale=200)
+        locs = vp.Points(intersection_points[0:1083,:], c='r')
+        # rays = vp.Arrows(origins, drays+start_point, c='b', scale=1000)
+        normal = vp.Arrows(start_points[0,:].reshape(-1,3), (face_normals[0,:]+start_points[0,:]).reshape(-1,3), c='g', scale=250)
+        vp.show(vp.trimesh2vtk(self.mesh).alpha(0.1).lw(0.1), locs, rays, normal, axes, axes=4)
+
+        # # for each hit, find the distance along its vector
+        # # you could also do this against the single camera Z vector
+        # depth = trimesh.util.diagonal_dot(intersection_points - start_point, drays[index_ray])
 
         return
 
@@ -139,8 +181,8 @@ class FormFactor(object):
 
         # # Parallel(n_jobs=5, prefer="threads")(delayed(self.__calculate_one_patch_form_factor)(i, p_1) for i, p_1 in enumerate(self.mesh.faces))
         #
-        # # for i, p_i in enumerate(self.mesh.faces):
-        # #     self.__calculate_one_patch_form_factor(i, p_i)
+        # for i, p_i in enumerate(self.mesh.faces):
+        #     self.__calculate_one_patch_form_factor(i, p_i)
         #
         # with ThreadPool(processes=processes) as pool:
         #     # for i, num in enumerate(np.arange(1,10)):

utils/rotation.py
@@ -2,7 +2,7 @@ import numpy as np
 # from typing import overload
 from multipledispatch import dispatch # for applying function overloading
 from multiprocessing.dummy import Pool as ThreadPool
-import mgen
+# import mgen
 import trimesh
 
 
@@ -110,6 +110,9 @@ def vrrotvec2mat(r, **kwargs):
 
      The rotation vector r is a row vector of 4 elements, where the first three elements specify the
      rotation axis and the last element defines the angle.
+     Notes
+     -----
+     Check on https://github.com/Wallacoloo/printipi/blob/master/util/rotation_matrix.py
     """
     processes = kwargs.pop('processes', 4)
     with ThreadPool(processes=processes) as pool:
@@ -140,3 +143,272 @@ def __compute_rotation_matrix(i,r):
     m[2, 1] = x * s + (1.0 - c) * y * z
     m[2, 2] = 1.0 + (1.0 - c) * (z ** 2 - 1.0)
     return m
+
+
+def rotation_matrices_from_angles(angles, directions):
+    r"""
+    Calculate a collection of rotation matrices defined by
+    an input collection of rotation angles and rotation axes.
+    Parameters
+    ----------
+    angles : ndarray
+        Numpy array of shape (npts, ) storing a collection of rotation angles
+    directions : ndarray
+        Numpy array of shape (npts, 3) storing a collection of rotation axes in 3d
+    Returns
+    -------
+    matrices : ndarray
+        Numpy array of shape (npts, 3, 3) storing a collection of rotation matrices
+    Examples
+    --------
+    Create a set of random rotation matrices.
+    >>> from halotools.utils.mcrotations import random_unit_vectors_3d
+    >>> npts = int(1e4)
+    >>> angles = np.random.uniform(-np.pi/2., np.pi/2., npts)
+    >>> directions = random_unit_vectors_3d(npts)
+    >>> rotation_matrices = rotation_matrices_from_angles(angles, directions)
+    Notes
+    -----
+    The function `rotate_vector_collection` can be used to efficiently
+    apply the returned collection of matrices to a collection of 3d vectors
+    check: https://github.com/astropy/halotools/blob/master/halotools/utils/rotations3d.py and https://stackoverflow.com/questions/47623582/efficiently-calculate-list-of-3d-rotation-matrices-in-numpy-or-scipy
+    """
+    directions = __normalized_vectors(directions)
+    angles = np.atleast_1d(angles)
+    npts = directions.shape[0]
+
+    sina = np.sin(angles)
+    cosa = np.cos(angles)
+
+    R1 = np.zeros((npts, 3, 3))
+    R1[:, 0, 0] = cosa
+    R1[:, 1, 1] = cosa
+    R1[:, 2, 2] = cosa
+
+    R2 = directions[..., None] * directions[:, None, :]
+    R2 = R2*np.repeat(1.-cosa, 9).reshape((npts, 3, 3))
+
+    directions *= sina.reshape((npts, 1))
+    R3 = np.zeros((npts, 3, 3))
+    R3[:, [1, 2, 0], [2, 0, 1]] -= directions
+    R3[:, [2, 0, 1], [1, 2, 0]] += directions
+
+    return R1 + R2 + R3
+
+def __normalized_vectors(vectors):
+    r"""
+    Return a unit-vector for each n-dimensional vector in the input list of n-dimensional points.
+    Parameters
+    ----------
+    x : ndarray
+        Numpy array of shape (npts, ndim) storing a collection of n-dimensional points
+    Returns
+    -------
+    normed_x : ndarray
+        Numpy array of shape (npts, ndim)
+    Examples
+    --------
+    Let's create a set of semi-random 3D unit vectors.
+    >>> npts = int(1e3)
+    >>> ndim = 3
+    >>> x = np.random.random((npts, ndim))
+    >>> normed_x = normalized_vectors(x)
+    """
+
+    vectors = np.atleast_2d(vectors)
+    npts = vectors.shape[0]
+
+    with np.errstate(divide='ignore', invalid='ignore'):
+        return vectors/__elementwise_norm(vectors).reshape((npts, -1))
+
+def __elementwise_norm(x):
+    r"""
+    Calculate the normalization of each element in a list of n-dimensional points.
+    Parameters
+    ----------
+    x : ndarray
+        Numpy array of shape (npts, ndim) storing a collection of n-dimensional points
+    Returns
+    -------
+    result : ndarray
+        Numpy array of shape (npts, ) storing the norm of each n-dimensional point in x.
+    Examples
+    --------
+    >>> npts = int(1e3)
+    >>> ndim = 3
+    >>> x = np.random.random((npts, ndim))
+    >>> norms = elementwise_norm(x)
+    """
+
+    x = np.atleast_2d(x)
+    return np.sqrt(np.sum(x**2, axis=1))
+
+
+def __elementwise_dot(x, y):
+    r"""
+    Calculate the dot product between
+    each pair of elements in two input lists of n-dimensional points.
+    Parameters
+    ----------
+    x : ndarray
+        Numpy array of shape (npts, ndim) storing a collection of n-dimensional vectors
+    y : ndarray
+        Numpy array of shape (npts, ndim) storing a collection of n-dimensional vectors
+    Returns
+    -------
+    result : ndarray
+        Numpy array of shape (npts, ) storing the dot product between each
+        pair of corresponding vectors in x and y.
+    Examples
+    --------
+    Let's create two sets of semi-random 3D vectors, x1 and x2.
+
+    >>> npts = int(1e3)
+    >>> ndim = 3
+    >>> x1 = np.random.random((npts, ndim))
+    >>> x2 = np.random.random((npts, ndim))
+    We then can find the dot product between each pair of vectors in x1 and x2.
+    >>> dots = elementwise_dot(x1, x2)
+    """
+
+    x = np.atleast_2d(x)
+    y = np.atleast_2d(y)
+    return np.sum(x * y, axis=1)
+
+
+def rotation_matrices_from_vectors(v0, v1):
+    r"""
+    Calculate a collection of rotation matrices defined by two sets of vectors,
+    v1 into v2, such that the resulting matrices rotate v1 into v2 about
+    the mutually perpendicular axis.
+    Parameters
+    ----------
+    v0 : ndarray
+        Numpy array of shape (npts, 3) storing a collection of initial vector orientations.
+        Note that the normalization of `v0` will be ignored.
+    v1 : ndarray
+        Numpy array of shape (npts, 3) storing a collection of final vectors.
+        Note that the normalization of `v1` will be ignored.
+    Returns
+    -------
+    matrices : ndarray
+        Numpy array of shape (npts, 3, 3) rotating each v0 into the corresponding v1
+    Examples
+    --------
+    Create a set of random rotation matrices.
+    >>> from halotools.utils.mcrotations import random_unit_vectors_3d
+    >>> npts = int(1e4)
+    >>> v0 = random_unit_vectors_3d(npts)
+    >>> v1 = random_unit_vectors_3d(npts)
+    >>> rotation_matrices = rotation_matrices_from_vectors(v0, v1)
+    Notes
+    -----
+    The function `rotate_vector_collection` can be used to efficiently
+    apply the returned collection of matrices to a collection of 3d vectors
+    """
+
+    # # View inputs as arrays with at least two dimensions.
+    # v0 = np.atleast_2d(v0)
+    # v1 = np.atleast_2d(v1)
+    #
+    # # check whether both inputs have the same dimensions, if not make them
+    # # TODO: maybe find a better way to check this
+    # if v0.shape[0] > v1.shape[0]:
+    #     v1 = np.tile(v1, (v0.shape[0], 1))
+    # elif v0.shape[0] < v1.shape[0]:
+    #     v0 = np.tile(v0, (v1.shape[0], 1))
+
+    # v0 = __normalized_vectors(v0)
+    # v1 = __normalized_vectors(v1)
+    # directions = __vectors_normal_to_planes(v0, v1)
+    # angles = __angles_between_list_of_vectors(v0, v1)
+    #
+    # # where angles are 0.0, replace directions with v0
+    # mask_a = (np.isnan(directions[:,0]) | np.isnan(directions[:,1]) | np.isnan(directions[:,2]))
+    # mask_b = (angles==0.0)
+    # mask = mask_a | mask_b
+    # directions[mask] = v0[mask]
+
+    # return rotation_matrices_from_angles(angles, directions)
+
+    rot = vvrotvec(v0, v1)
+    return rotation_matrices_from_angles(rot[:,3], rot[:,0:3])
+
+
+def __angles_between_list_of_vectors(v0, v1, tol=1e-3, vn=None):
+    r""" Calculate the angle between a collection of n-dimensional vectors
+    Parameters
+    ----------
+    v0 : ndarray
+        Numpy array of shape (npts, ndim) storing a collection of ndim-D vectors
+        Note that the normalization of `v0` will be ignored.
+    v1 : ndarray
+        Numpy array of shape (npts, ndim) storing a collection of ndim-D vectors
+        Note that the normalization of `v1` will be ignored.
+    tol : float, optional
+        Acceptable numerical error for errors in angle.
+        This variable is only used to round off numerical noise that otherwise
+        causes exceptions to be raised by the inverse cosine function.
+        Default is 0.001.
+    n1 : ndarray
+        normal vector
+    Returns
+    -------
+    angles : ndarray
+        Numpy array of shape (npts, ) storing the angles between each pair of
+        corresponding points in v0 and v1.
+        Returned values are in units of radians spanning [0, pi].
+    Examples
+    --------
+    Let's create two sets of semi-random 3D unit vectors.
+    >>> npts = int(1e4)
+    >>> ndim = 3
+    >>> v1 = np.random.random((npts, ndim))
+    >>> v2 = np.random.random((npts, ndim))
+    We then can find the angle between each pair of vectors in v1 and v2.
+    >>> angles = angles_between_list_of_vectors(v1, v2)
+    """
+
+    dot = __elementwise_dot(__normalized_vectors(v0), __normalized_vectors(v1))
+
+    if vn is None:
+        #  Protect against tiny numerical excesses beyond the range [-1 ,1]
+        mask1 = (dot > 1) & (dot < 1 + tol)
+        dot = np.where(mask1, 1., dot)
+        mask2 = (dot < -1) & (dot > -1 - tol)
+        dot = np.where(mask2, -1., dot)
+        a = np.arccos(dot)
+    else:
+        cross = np.cross(v0, v1)
+        a = np.arctan2(__elementwise_dot(cross, vn), dot)
+
+    return a
+
+
+def __vectors_normal_to_planes(x, y):
+    r"""
+    Given a collection of 3d vectors x and y, return a collection of
+    3d unit-vectors that are orthogonal to x and y.
+    Parameters
+    ----------
+    x : ndarray
+        Numpy array of shape (npts, 3) storing a collection of 3d vectors
+        Note that the normalization of `x` will be ignored.
+    y : ndarray
+        Numpy array of shape (npts, 3) storing a collection of 3d vectors
+        Note that the normalization of `y` will be ignored.
+    Returns
+    -------
+    z : ndarray
+        Numpy array of shape (npts, 3). Each 3d vector in z will be orthogonal
+        to the corresponding vector in x and y.
+    Examples
+    --------
+    Define a set of random 3D vectors
+    >>> npts = int(1e4)
+    >>> x = np.random.random((npts, 3))
+    >>> y = np.random.random((npts, 3))
+    now calculate a thrid set of vectors to a corresponding pair in `x` and `y`.
+    >>> normed_z = angles_between_list_of_vectors(x, y)
+    """
+    return __normalized_vectors(np.cross(x, y))